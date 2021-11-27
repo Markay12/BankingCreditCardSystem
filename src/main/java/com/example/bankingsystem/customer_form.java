2f019dc89e132fe81f232da0b5b940024e30e41a
Markay <mashinhu@asu.edu>
2021-11-27 09:25:46 -0700

    Added error checking and duplicate id's

--- src/main/java/com/example/bankingsystem/customer_form.java
+++ src/main/java/com/example/bankingsystem/customer_form.java
@@ -12,6 +12,7 @@ import java.nio.Buffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class customer_form {
 
@@ -19,9 +20,13 @@ public class customer_form {
     private TextField fname_field, lname_field, address_field, number_field, custid_field, saccnt_field, caccnt_field;
 
     @FXML
-    private Label appSentLabel;
+    private Label appSentLabel, errorLabel;
 
     private int fileNum = 0;
+
+    //list to hold all customer Id information
+    private int idCount = 0;
+    private ArrayList<String> idList = new ArrayList<String>();
 
     public void erase(ActionEvent actionEvent) throws IOException
     {
@@ -40,49 +45,168 @@ public class customer_form {
     public void saveToFile(ActionEvent actionEvent) throws IOException
     {
 
-        //save information to a file
-        try
+        boolean canSave = true;
+
+        //error and type checking
+        if (fname_field.getText().isEmpty())
+        {
+            errorLabel.setText("Please enter a first name.");
+            errorLabel.setVisible(true);
+            canSave = false;
+
+        }
+        else if (lname_field.getText().isEmpty())
         {
 
-            fileNum++;
+            errorLabel.setText("Please enter a last name.");
+            errorLabel.setVisible(true);
+            canSave = false;
 
-            FileWriter writer = new FileWriter("customer_information\\customer" + fileNum + "Information.txt");
+        }
+        else if (custid_field.getText().isEmpty())
+        {
 
-            //create string to write to text File
-            String customer_info = "";
+            errorLabel.setText("Please enter a customer ID.");
+            errorLabel.setVisible(true);
+            canSave = false;
 
-            customer_info += "First name: " + fname_field.getText() + "\nLast name: " + lname_field.getText() + "\nCustomer ID: " + custid_field.getText() + "\nPhone Number: " + number_field.getText()
-            + "\nAddress: " + address_field.getText() + "\nSavings Account #: " + saccnt_field.getText() + "\tChecking Account #: " + caccnt_field.getText() + "\n\n";
+        }
+        else if (custid_field.getText().length() != 4)
+        {
+            if (custid_field.getText().length() > 4) {
+
+                errorLabel.setText("Customer ID is too long.");
+                errorLabel.setVisible(true);
+                canSave = false;
+            }
+            else
+            {
+                errorLabel.setText("Customer ID is too short.");
+                errorLabel.setVisible(true);
+                canSave = false;
+            }
+
+        }
+        else if (number_field.getText().isEmpty())
+        {
+
+            errorLabel.setText("Please enter a phone number.");
+            errorLabel.setVisible(true);
+            canSave = false;
+
+        }
+        else if (number_field.getText().length() != 10)
+        {
+
+            errorLabel.setText("Please enter a valid phone number.");
+            errorLabel.setVisible(true);
+            canSave = false;
+
+        }
+        else if (address_field.getText().isEmpty())
+        {
+
+            errorLabel.setText("Please enter an address.");
+            errorLabel.setVisible(true);
+            canSave = false;
+
+        }
+        else if (saccnt_field.getText().isEmpty())
+        {
+
+            errorLabel.setText("Please enter a savings account number.");
+            errorLabel.setVisible(true);
+            canSave = false;
+
+        }
+        else if (caccnt_field.getText().isEmpty())
+        {
+
+            errorLabel.setText("Please enter a checking account number.");
+            errorLabel.setVisible(true);
+            canSave = false;
+
+        }
+
+        boolean badId = false;
+
+        for (int i = 0; i < idCount; i++)
+        {
+
+            if (custid_field.getText().equals(idList.get(i)))
+            {
+
+                badId = true;
+                continue;
+
+            }
+
+        }
 
 
-            //write to file
-            writer.write(customer_info);
-            writer.close();
+        if (badId)
+        {
+
+            errorLabel.setText("Duplicate user id entered.");
+            errorLabel.setVisible(true);
+            badId = false;
+            canSave = false;
+
+        }
+
+
+        if (canSave) {
+
+
+            //save information to a file
+            try {
+
+                fileNum++;
+
+                FileWriter writer = new FileWriter("customer" + fileNum + "Information.txt");
+
+                //create string to write to text File
+                String customer_info = "";
+
+                customer_info += "First name: " + fname_field.getText() + "\nLast name: " + lname_field.getText() + "\nCustomer ID: " + custid_field.getText() + "\nPhone Number: " + number_field.getText()
+                        + "\nAddress: " + address_field.getText() + "\nSavings Account #: " + saccnt_field.getText() + "\tChecking Account #: " + caccnt_field.getText() + "\n\n";
+
+
+                //write to file
+                writer.write(customer_info);
+
+                //add customer id to list
+                idList.add(custid_field.getText());
+                //increment amount of id's
+                idCount++;
+
+                //stop showing previous error label
+                errorLabel.setVisible(false);
+
+                writer.close();
+
+
+            } catch (IOException e) {
+
+                System.out.println("Error opening/writing to file");
+                e.printStackTrace();
+
+            }
+
+
+            //clear information and update success label
+            fname_field.setText("");
+            lname_field.setText("");
+            address_field.setText("");
+            number_field.setText("");
+            custid_field.setText("");
+            saccnt_field.setText("");
+            caccnt_field.setText("");
+
+            appSentLabel.setVisible(true);
 
 
         }
-        catch (IOException e)
-        {
-
-            System.out.println("Error opening/writing to file");
-            e.printStackTrace();
-
-        }
-
-
-        //clear information and update success label
-        fname_field.setText("");
-        lname_field.setText("");
-        address_field.setText("");
-        number_field.setText("");
-        custid_field.setText("");
-        saccnt_field.setText("");
-        caccnt_field.setText("");
-
-        appSentLabel.setVisible(true);
-
-
-
     }
 
 
